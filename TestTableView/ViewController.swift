8266a5f670e8e631ebc85a3ded71278a49311318
粘光裕 <tgnivekucn@gmail.com>
2018-10-30 23:36:57 +0800

    add swipe delete & reorder functions

--- TestTableView/ViewController.swift
+++ TestTableView/ViewController.swift
@@ -25,13 +25,17 @@ class CustomTableViewCell: UITableViewCell {
 
 class ViewController: UIViewController {
     @IBOutlet weak var mTableView: UITableView!
-    let list = ["Kevin","Mary","Jay","Steven"]
+    var list = ["Kevin","Mary","Jay","Steven"]
     override func viewDidLoad() {
         super.viewDidLoad()
         //set ViewController as UITableViewDelegate's Delegate
         mTableView.delegate = self
         //set ViewController as UITableViewDataSource's Delegate
         mTableView.dataSource = self
+        
+        //應有一個按鈕來修改這個flag的Bool值,true就是代表在編輯模式下(可以重新排序),false表示在非編輯模式(可以左滑刪除)
+        mTableView.isEditing = false
+
     }
 }
 
@@ -48,5 +52,35 @@ extension ViewController: UITableViewDataSource {
 }
 
 extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .default, title: "刪除") {
+            action, index in
+            self.list.remove(at: index.row)
+            self.mTableView.reloadData()
+        }
+        return [deleteAction]
+    }
+    
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if mTableView.isEditing {
+            return .none
+        } else {
+            return .delete
+        }
+    }
+
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = list[sourceIndexPath.row]
+        list.remove(at: sourceIndexPath.row)
+        list.insert(itemToMove, at: destinationIndexPath.row)
+    }
+    
+    
     
 }
